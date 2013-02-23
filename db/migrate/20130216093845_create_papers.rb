df88e1d664acea5595a8b33d42a87224bfd6925c
Mohamed Shehata <mshehata@mshehata.(none)>
2013-02-23 18:35:25 +0200

    some modifications

--- db/migrate/20130216093845_create_papers.rb
+++ db/migrate/20130216093845_create_papers.rb
@@ -3,7 +3,7 @@ class CreatePapers < ActiveRecord::Migration
     create_table :papers do |t|
       t.string :title
       t.string :auth1
-      t.srting :auth2
+      t.string :auth2
       t.string :auth3
       t.string :auth4
       t.text :abstract
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130215200822) do
+ActiveRecord::Schema.define(:version => 20130216100137) do
+
+  create_table "papers", :force => true do |t|
+    t.string   "title"
+    t.string   "auth1"
+    t.string   "auth2"
+    t.string   "auth3"
+    t.string   "auth4"
+    t.text     "abstract"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
